b3438da9813f09d1531a38a163d3cadcdcb045d1
AnaJulia1906 <anajuliarh19@gmail.com>
2024-11-07 17:32:04 -0300

    Create pizzas.js

---
+++ pizzas.js
@@ -0,0 +1,147 @@
+pizzaJson=[
+    {
+      id: 1,
+      name: 'Penne con salsa bianca.',
+      img: 'imagem/penne com molho branco...jpeg',
+      price: [49.90, 69.90, 149.99],
+      sizes: [
+        '1 porção',
+        '2 porções',
+        '3 porções'
+      ],
+      description: 'Massa: Penne, com molho branco, queijo parmessão e torradas'
+    },
+    {
+      id: 2,
+      name: 'Carbonara',
+      img: 'imagem/carbonara.jpeg',
+      price: [45.99, 60.00, 139.99],
+      sizes: [
+        '1 porção',
+        '2 porções',
+        '3 porções'
+      ],
+      description: 'Massa: Talharim, com molho carbonara, pedaços de bacon e uma gema de ovo crua'
+    },
+    {
+      id: 3,
+      name: 'Tagliatelle con salsa bianca e spezie.',
+      img: 'imagem/talharim com molho branco.jpeg',
+      price: [40.00, 55.00, 95.00],
+      sizes: [
+        '1 porção',
+        '2 porções',
+        '3 porções'
+      ],
+      description: 'Massa: Talharim, com molho branco e especiarias'
+    },
+    {
+      id: 4,
+      name: 'Brasileira',
+      img: 'images/brasileira.jpg',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+    },
+    {
+      id: 5,
+      name: 'Portuguesa',
+      img: 'images/portuguesa.jpg',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'
+    },
+    {
+      id: 6,
+      name: 'Moda da Casa',
+      img: 'images/da casa.jpg',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+    },
+    {
+      id: 7,
+      name: 'Banana com canela',
+      img: 'images/banana.jpg',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Mussarela, banana, canela e açúcar'
+    },
+    {
+      id: 8,
+      name: 'Chocolate com morango',
+      img: 'images/morango.jpg',
+      price: [20.00, 23.00, 25.00],
+      sizes: [
+        '6 fatias',
+        '8 fatias',
+        '12 fatias'
+      ],
+      description: 'Creme de leite, lascas de chocolate e morangos'
+    },
+    {
+        id: 9,
+        name: 'Chocolate com morango',
+        img: 'images/morango.jpg',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Creme de leite, lascas de chocolate e morangos'
+      },
+      {
+        id: 10,
+        name: 'Chocolate com morango',
+        img: 'images/morango.jpg',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Creme de leite, lascas de chocolate e morangos'
+      },
+      {
+        id: 11,
+        name: 'Chocolate com morango',
+        img: 'images/morango.jpg',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Creme de leite, lascas de chocolate e morangos'
+      },
+      {
+        id: 12,
+        name: 'Chocolate com morango',
+        img: 'images/morango.jpg',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Creme de leite, lascas de chocolate e morangos'
+      }
+      
+  ];
